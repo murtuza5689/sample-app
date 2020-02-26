f72c0718e1080ba2ee694b3dde72ed10b29d6288
Murtuza Mithaiwala <murtuza5689@gmail.com>
2020-02-26 10:00:03 +0530

    changes to dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h3 style="text-align: center;">Hi, there!</h3>'
 })
 export class AppComponent {
   title = 'sample-app';
